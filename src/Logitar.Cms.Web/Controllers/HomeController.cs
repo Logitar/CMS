d32d792c4ab1723e0b4fc1a20d5f0a9bdb588814
Francis Pion <francispion@hotmail.com>
2023-04-24 19:17:46 -0400

    Frontend-into-backend integration.

---
+++ src/Logitar.Cms.Web/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Logitar.Cms.Web.Controllers;
+
+[ApiExplorerSettings(IgnoreApi = true)]
+[Route("cms")]
+public class HomeController : Controller
+{
+  [HttpGet]
+  public ActionResult Index() => View();
+}
