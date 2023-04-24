233844a513e36a7c26075997bc3b5a71ed8794e3
Raphaël Pion <raph.pion@gmail.com>
2023-04-24 12:05:37 -0400

    #1 Directory structure

--- src/Logitar.Cms.Ui/vite.config.ts
+++ src/Logitar.Cms.Ui/vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigpaths from "vite-tsconfig-paths";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tsconfigpaths()],
+  resolve: {
+    alias: [
+      { find: "~api", replacement: path.resolve(__dirname, "api") },
+      { find: "~components", replacement: path.resolve(__dirname, "components") },
+      { find: "~hooks", replacement: path.resolve(__dirname, "hooks") },
+      { find: "~models", replacement: path.resolve(__dirname, "models") },
+      { find: "~pages", replacement: path.resolve(__dirname, "pages") },
+    ],
+  },
+});
