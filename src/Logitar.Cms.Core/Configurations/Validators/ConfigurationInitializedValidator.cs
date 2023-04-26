3690d235d35425eeaed6aec2f0ba72e06f761a76
Francis Pion <francis.pion@akinox.com>
2023-04-26 14:38:43 -0400

    Removed magic numbers.

--- src/Logitar.Cms.Core/Configurations/Validators/ConfigurationInitializedValidator.cs
+++ src/Logitar.Cms.Core/Configurations/Validators/ConfigurationInitializedValidator.cs
@@ -5,11 +5,14 @@ namespace Logitar.Cms.Core.Configurations.Validators;
 
 internal class ConfigurationInitializedValidator : AbstractValidator<ConfigurationInitialized>
 {
+  private const int MinimumSecretLength = 256 / 8; // JSON Web Tokens symmetric signing keys should use at least 256 bits. Reference: https://web-token.spomky-labs.com/advanced-topics-1/security-recommendations
+  private const int MaximumSecretLength = 512 / 8; // JSON Web Tokens symmetric signing keys should not be too long. We will be using the HS256 algorithm for now.
+
   public ConfigurationInitializedValidator()
   {
     RuleFor(x => x.Secret).NotEmpty()
-      .MinimumLength(256 / 8)
-      .MaximumLength(512 / 8);
+      .MinimumLength(MinimumSecretLength)
+      .MaximumLength(MaximumSecretLength);
 
     RuleFor(x => x.LoggingSettings).NotNull()
       .SetValidator(new ReadOnlyLoggingSettingsValidator());
