4ee7464c74d4453367249facb637d1f289341184
Raphaël Pion <raph.pion@gmail.com>
2023-05-03 21:14:29 -0400

    Fix invalid env

--- src/Logitar.Cms.Ui/locales/index.ts
+++ src/Logitar.Cms.Ui/locales/index.ts
@@ -2,7 +2,7 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-const isProduction = import.meta.env.NODE_ENV === 'production';
+const isProduction = import.meta.env.MODE === 'production';
 
 import en from './en';
 import fr from './fr';
