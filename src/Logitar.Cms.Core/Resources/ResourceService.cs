0170d6a2e3a8a102aba0266d283ef87101ca5df0
Raphaël Pion <raph.pion@gmail.com>
2023-05-08 22:32:51 -0400

    Fix

--- src/Logitar.Cms.Core/Resources/ResourceService.cs
+++ src/Logitar.Cms.Core/Resources/ResourceService.cs
@@ -6,7 +6,6 @@ namespace Logitar.Cms.Core.Resources;
 internal class ResourceService : IResourceService
 {
   private static readonly IEnumerable<Locale> _locales = CultureInfo.GetCultures(CultureTypes.AllCultures)
-    .Select(Locale.From).OrderBy(x => x.NativeName);
     .Where(culture => !string.IsNullOrWhiteSpace(culture.Name) && culture.LCID != 4096)
     .Select(Locale.From)
     .OrderBy(x => x.NativeName);
