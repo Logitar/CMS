833b5c688dfeffa011f321d2de4f17305a69c5a9
Raphaël Pion <raph.pion@gmail.com>
2023-05-09 20:56:47 -0400

    Create CurrentUser type

--- src/Logitar.Cms.Ui/models/index.ts
+++ src/Logitar.Cms.Ui/models/index.ts
@@ -2,4 +2,5 @@ export * from './enums';
 export * from './payloads';
 
 export type { ApiErrorResponse } from './ApiErrorResponse';
+export type { CurrentUser } from './CurrentUser';
 export type { Locale } from './Locale';
