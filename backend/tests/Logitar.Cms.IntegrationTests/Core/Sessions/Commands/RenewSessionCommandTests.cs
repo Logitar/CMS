f6715e0455f90206019f24f6019deff2b198d932
Francis Pion <francispion@hotmail.com>
2024-03-24 16:07:55 -0400

    Added integration tests skeleton.

---
+++ backend/tests/Logitar.Cms.IntegrationTests/Core/Sessions/Commands/RenewSessionCommandTests.cs
@@ -0,0 +1,11 @@
+﻿namespace Logitar.Cms.Core.Sessions.Commands;
+
+[Trait(Traits.Category, Categories.Integration)]
+public class RenewSessionCommandTests : IntegrationTests
+{
+  public RenewSessionCommandTests() : base()
+  {
+  }
+
+  // TODO(fpion): implement
+}
---
+++ backend/tests/Logitar.Cms.IntegrationTests/Core/Users/Commands/AuthenticateUserCommandTests.cs
@@ -0,0 +1,11 @@
+﻿namespace Logitar.Cms.Core.Users.Commands;
+
+[Trait(Traits.Category, Categories.Integration)]
+public class AuthenticateUserCommandTests : IntegrationTests
+{
+  public AuthenticateUserCommandTests() : base()
+  {
+  }
+
+  // TODO(fpion): implement
+}
