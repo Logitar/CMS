2fc5525af97fedd27310c1d5717882ee300b6e92
Francis Pion <francispion@hotmail.com>
2024-06-07 01:03:25 -0400

    Added account contracts.

---
+++ backend/src/Logitar.Cms.Contracts/Users/Contact.cs
@@ -0,0 +1,10 @@
+﻿using Logitar.Cms.Contracts.Actors;
+
+namespace Logitar.Cms.Contracts.Users;
+
+public abstract record Contact
+{
+  public bool IsVerified { get; set; }
+  public Actor? VerifiedBy { get; set; }
+  public DateTime? VerifiedOn { get; set; }
+}
