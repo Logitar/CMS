99a838728efe93c23db6ef59073fb20e2db67611
Francis Pion <francispion@hotmail.com>
2024-06-06 23:30:36 -0400

    Contracts

---
+++ backend/src/Logitar.Cms.Contracts/Errors/ValidationError.cs
@@ -0,0 +1,21 @@
+﻿namespace Logitar.Cms.Contracts.Errors;
+
+public record ValidationError : Error
+{
+  public List<PropertyError> Errors { get; set; }
+
+  public ValidationError() : this("Validation", "Validation failed.")
+  {
+  }
+
+  public ValidationError(string code, string message) : this(code, message, [])
+  {
+  }
+
+  public ValidationError(string code, string message, IEnumerable<ErrorData> data) : base(code, message, data)
+  {
+    Errors = [];
+  }
+
+  public void Add(PropertyError error) => Errors.Add(error);
+}
---
+++ backend/src/Logitar.Cms.Contracts/Locale.cs
@@ -0,0 +1,27 @@
+﻿namespace Logitar.Cms.Contracts;
+
+public record Locale
+{
+  public int Id { get; set; }
+  public string Code { get; set; }
+  public string DisplayName { get; set; }
+  public string EnglishName { get; set; }
+  public string NativeName { get; set; }
+
+  public Locale() : this(string.Empty)
+  {
+  }
+
+  public Locale(string code) : this(CultureInfo.GetCultureInfo(code))
+  {
+  }
+
+  public Locale(CultureInfo culture)
+  {
+    Id = culture.LCID;
+    Code = culture.Name;
+    DisplayName = culture.DisplayName;
+    EnglishName = culture.EnglishName;
+    NativeName = culture.NativeName;
+  }
+}
