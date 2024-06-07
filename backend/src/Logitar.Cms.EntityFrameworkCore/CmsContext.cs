3a6eeaa4efc29620b949b273b086df54c5fcd0e4
Francis Pion <francispion@hotmail.com>
2024-06-06 23:58:00 -0400

    Persistence layers.

---
+++ backend/src/Logitar.Cms.EntityFrameworkCore/CmsContext.cs
@@ -0,0 +1,18 @@
+﻿using Logitar.Cms.EntityFrameworkCore.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Logitar.Cms.EntityFrameworkCore;
+
+public class CmsContext : DbContext
+{
+  public CmsContext(DbContextOptions<CmsContext> options) : base(options)
+  {
+  }
+
+  internal DbSet<LanguageEntity> Languages { get; private set; }
+
+  protected override void OnModelCreating(ModelBuilder modelBuilder)
+  {
+    modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+  }
+}
---
+++ backend/src/Logitar.Cms.EntityFrameworkCore/Configurations/LanguageConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Logitar.Cms.EntityFrameworkCore.Entities;
+using Logitar.Identity.Domain.Shared;
+using Logitar.Identity.EntityFrameworkCore.Relational.Configurations;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Logitar.Cms.EntityFrameworkCore.Configurations;
+
+internal class LanguageConfiguration : AggregateConfiguration<LanguageEntity>, IEntityTypeConfiguration<LanguageEntity>
+{
+  public override void Configure(EntityTypeBuilder<LanguageEntity> builder)
+  {
+    base.Configure(builder);
+
+    builder.ToTable(nameof(CmsContext.Languages));
+    builder.HasKey(x => x.LanguageId);
+
+    builder.HasIndex(x => x.IsDefault); // ISSUE: https://github.com/Logitar/CMS/issues/10
+    builder.HasIndex(x => x.Locale);
+    builder.HasIndex(x => x.LocaleNormalized).IsUnique();
+
+    builder.Property(x => x.Locale).HasMaxLength(LocaleUnit.MaximumLength);
+    builder.Property(x => x.LocaleNormalized).HasMaxLength(LocaleUnit.MaximumLength);
+  }
+}
