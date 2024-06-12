dfe40b730166cc4414cfe261ca045550a3259e56
Francis Pion <francispion@hotmail.com>
2024-06-12 12:18:25 -0400

    Controller.

---
+++ backend/src/Logitar.Cms.Web/Controllers/ContentController.cs
@@ -0,0 +1,32 @@
+﻿using Logitar.Cms.Contracts.Contents;
+using Logitar.Cms.Core;
+using Logitar.Cms.Core.Contents.Commands;
+using Logitar.Cms.Web.Extensions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Logitar.Cms.Web.Controllers;
+
+[ApiController]
+[Authorize]
+[Route("api/contents")]
+public class ContentController : ControllerBase
+{
+  private readonly IRequestPipeline _pipeline;
+
+  public ContentController(IRequestPipeline pipeline)
+  {
+    _pipeline = pipeline;
+  }
+
+  [HttpPost]
+  public async Task<ActionResult<ContentItem>> CreateAsync([FromBody] CreateContentPayload payload, CancellationToken cancellationToken)
+  {
+    ContentItem content = await _pipeline.ExecuteAsync(new CreateContentCommand(payload), cancellationToken);
+    Uri location = HttpContext.BuildLocation("api/contents/{id}", new Dictionary<string, string>
+    {
+      ["id"] = content.Id.ToString()
+    });
+    return Created(location, content);
+  }
+}
