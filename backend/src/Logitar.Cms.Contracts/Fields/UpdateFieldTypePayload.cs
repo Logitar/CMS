ac763d17ae18aaf47cdcf5582c36cda7466b2b6a
Francis Pion <francispion@hotmail.com>
2024-06-23 11:27:46 -0400

    Implemented field type update.

---
+++ backend/src/Logitar.Cms.Contracts/Fields/UpdateFieldTypePayload.cs
@@ -0,0 +1,17 @@
+﻿using Logitar.Cms.Contracts.Fields.Properties;
+using Logitar.Identity.Contracts;
+
+namespace Logitar.Cms.Contracts.Fields;
+
+public record UpdateFieldTypePayload
+{
+  public string? UniqueName { get; set; }
+  public Modification<string>? DisplayName { get; set; }
+  public Modification<string>? Description { get; set; }
+
+  public BooleanProperties? BooleanProperties { get; set; }
+  public DateTimeProperties? DateTimeProperties { get; set; }
+  public NumberProperties? NumberProperties { get; set; }
+  public StringProperties? StringProperties { get; set; }
+  public TextProperties? TextProperties { get; set; }
+}
