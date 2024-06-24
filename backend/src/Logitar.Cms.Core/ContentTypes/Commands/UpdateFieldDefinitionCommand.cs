89a3526d541991643c87846304616e357703d40c
Francis Pion <francispion@hotmail.com>
2024-06-24 19:41:44 -0400

    Implemented field definition update. (#53)

---
+++ backend/src/Logitar.Cms.Core/ContentTypes/Commands/UpdateFieldDefinitionCommand.cs
@@ -0,0 +1,6 @@
+﻿using Logitar.Cms.Contracts.ContentTypes;
+using MediatR;
+
+namespace Logitar.Cms.Core.ContentTypes.Commands;
+
+public record UpdateFieldDefinitionCommand(Guid ContentTypeId, Guid FieldId, UpdateFieldDefinitionPayload Payload) : Activity, IRequest<ContentsType?>;
