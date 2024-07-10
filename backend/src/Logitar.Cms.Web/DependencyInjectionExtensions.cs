4ac45526bca0fbfaece681a7af6659cb1ef578bf
Francis Pion <francispion@hotmail.com>
2024-07-10 14:47:58 -0400

    Web interface.

--- backend/src/Logitar.Cms.Web/DependencyInjectionExtensions.cs
+++ backend/src/Logitar.Cms.Web/DependencyInjectionExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Logitar.Cms.Core;
 using Logitar.Cms.Web.Filters;
+using Logitar.Cms.Web.Settings;
 
 namespace Logitar.Cms.Web;
 
@@ -11,7 +12,7 @@ public static class DependencyInjectionExtensions
       .AddControllersWithViews(options =>
       {
         options.Filters.Add<ExceptionHandling>();
-        // TODO(fpion): LoggingFilter
+        options.Filters.Add<LoggingFilter>();
       })
       .AddJsonOptions(options =>
       {
@@ -19,6 +20,15 @@ public static class DependencyInjectionExtensions
         options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
       });
 
-    return services.AddLogitarCmsCore();
+    return services
+      .AddLogitarCmsCore()
+      .AddSingleton(GetCookiesSettings)
+      .AddSingleton<IActivityContextResolver, HttpActivityContextResolver>();
+  }
+
+  public static CookiesSettings GetCookiesSettings(this IServiceProvider serviceProvider)
+  {
+    IConfiguration configuration = serviceProvider.GetRequiredService<IConfiguration>();
+    return configuration.GetSection(CookiesSettings.SectionKey).Get<CookiesSettings>() ?? new();
   }
 }
---
+++ backend/src/Logitar.Cms.Web/HttpActivityContextResolver.cs
@@ -0,0 +1,26 @@
+﻿using Logitar.Cms.Contracts.Configurations;
+using Logitar.Cms.Core;
+using Logitar.Cms.Core.Caching;
+
+namespace Logitar.Cms.Web;
+
+public class HttpActivityContextResolver : IActivityContextResolver
+{
+  private readonly ICacheService _cacheService;
+  private readonly IHttpContextAccessor _httpContextAccessor;
+
+  private Configuration Configuration => _cacheService.Configuration ?? throw new InvalidOperationException("The configuration was not found in the cache.");
+  private HttpContext HttpContext => _httpContextAccessor.HttpContext ?? throw new InvalidOperationException($"The {nameof(_httpContextAccessor.HttpContext)} is required.");
+
+  public HttpActivityContextResolver(ICacheService cacheService, IHttpContextAccessor httpContextAccessor)
+  {
+    _cacheService = cacheService;
+    _httpContextAccessor = httpContextAccessor;
+  }
+
+  public Task<ActivityContext> ResolveAsync(CancellationToken cancellationToken)
+  {
+    ActivityContext context = new(Configuration, HttpContext.GetApiKey(), HttpContext.GetSession(), HttpContext.GetUser());
+    return Task.FromResult(context);
+  }
+}
