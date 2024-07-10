c10d1a918d1a2061095e7eba4fbcb1e15395238c
Francis Pion <francispion@hotmail.com>
2024-07-10 19:16:49 -0400

    Core layer.

---
+++ backend/src/Logitar.Cms.Core/Sessions/ISessionQuerier.cs
@@ -0,0 +1,11 @@
+﻿using Logitar.Cms.Contracts.Sessions;
+using Logitar.Identity.Domain.Sessions;
+
+namespace Logitar.Cms.Core.Sessions;
+
+public interface ISessionQuerier
+{
+  Task<Session> ReadAsync(SessionAggregate session, CancellationToken cancellationToken = default);
+  Task<Session?> ReadAsync(SessionId id, CancellationToken cancellationToken = default);
+  Task<Session?> ReadAsync(Guid id, CancellationToken cancellationToken = default);
+}
---
+++ backend/src/Logitar.Cms.Core/Sessions/Validators/SignInSessionValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using Logitar.Cms.Contracts.Sessions;
+using Logitar.Cms.Core.Validators;
+
+namespace Logitar.Cms.Core.Sessions.Validators;
+
+public class SignInSessionValidator : AbstractValidator<SignInSessionPayload>
+{
+  public SignInSessionValidator()
+  {
+    RuleFor(x => x.UniqueName).NotEmpty();
+    RuleFor(x => x.Password).NotEmpty();
+
+    RuleForEach(x => x.CustomAttributes).SetValidator(new ContractCustomAttributeValidator());
+  }
+}
