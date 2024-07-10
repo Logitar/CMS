ecf8d88a67ba4e351e4068d36fff1b2c5be144a2
Francis Pion <francispion@hotmail.com>
2024-07-10 11:39:11 -0400

    Infrastructure layer.

---
+++ backend/src/Logitar.Cms.Infrastructure/Caching/CacheService.cs
@@ -0,0 +1,26 @@
+﻿using Logitar.Cms.Contracts.Configurations;
+using Logitar.Cms.Core.Caching;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace Logitar.Cms.Infrastructure.Caching;
+
+internal class CacheService : ICacheService
+{
+  private const string ConfigurationKey = nameof(Configuration);
+
+  private readonly IMemoryCache _cache;
+
+  public CacheService(IMemoryCache cache)
+  {
+    _cache = cache;
+  }
+
+  public Configuration? Configuration
+  {
+    get => GetItem<Configuration>(ConfigurationKey);
+    set => SetItem(ConfigurationKey, value);
+  }
+
+  private T? GetItem<T>(object key) => _cache.TryGetValue(key, out T? value) ? value : default;
+  private void SetItem<T>(object key, T value) => _cache.Set(key, value);
+}
---
+++ backend/src/Logitar.Cms.Infrastructure/DependencyInjectionExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Logitar.Cms.Core;
+using Logitar.Cms.Core.Caching;
+using Logitar.Cms.Infrastructure.Caching;
+using Logitar.Cms.Infrastructure.Converters;
+using Logitar.EventSourcing.Infrastructure;
+using Logitar.Identity.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Logitar.Cms.Infrastructure;
+
+public static class DependencyInjectionExtensions
+{
+  public static IServiceCollection AddLogitarCmsInfrastructure(this IServiceCollection services)
+  {
+    return services
+      .AddLogitarIdentityInfrastructure()
+      .AddLogitarCmsCore()
+      .AddMemoryCache()
+      .AddSingleton<ICacheService, CacheService>()
+      .AddSingleton<IEventSerializer>(services => new EventSerializer(services.GetLogitarCmsJsonConverters()));
+  }
+
+  public static IReadOnlyCollection<JsonConverter> GetLogitarCmsJsonConverters(this IServiceProvider serviceProvider)
+  {
+    List<JsonConverter> converters = [];
+    converters.AddRange(serviceProvider.GetLogitarIdentityJsonConverters());
+
+    converters.Add(new ConfigurationIdConverter());
+    converters.Add(new JwtSecretConverter());
+    converters.Add(new LanguageIdConverter());
+
+    return converters.AsReadOnly();
+  }
+}
