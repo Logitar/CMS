0fad6b94c03f0cfc3611cd2d09bf98aa6fd6e290
Francis Pion <francispion@hotmail.com>
2024-07-10 16:14:26 -0400

    Contracts.

---
+++ backend/src/Logitar.Cms.Contracts/Languages/CreateLanguagePayload.cs
@@ -0,0 +1,15 @@
+﻿namespace Logitar.Cms.Contracts.Languages;
+
+public record CreateLanguagePayload
+{
+  public string Locale { get; set; }
+
+  public CreateLanguagePayload() : this(string.Empty)
+  {
+  }
+
+  public CreateLanguagePayload(string locale)
+  {
+    Locale = locale;
+  }
+}
