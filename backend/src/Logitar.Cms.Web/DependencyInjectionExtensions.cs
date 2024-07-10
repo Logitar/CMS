786d2c12705fc637ef8b98ec15a5f0f0938bb63e
Francis Pion <francispion@hotmail.com>
2024-07-10 13:16:27 -0400

    Web interface.

---
+++ backend/src/Logitar.Cms.Web/DependencyInjectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Logitar.Cms.Core;
+using Logitar.Cms.Web.Filters;
+
+namespace Logitar.Cms.Web;
+
+public static class DependencyInjectionExtensions
+{
+  public static IServiceCollection AddLogitarCmsWeb(this IServiceCollection services)
+  {
+    services
+      .AddControllersWithViews(options =>
+      {
+        options.Filters.Add<ExceptionHandling>();
+        // TODO(fpion): LoggingFilter
+      })
+      .AddJsonOptions(options =>
+      {
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+      });
+
+    return services.AddLogitarCmsCore();
+  }
+}
---
+++ backend/src/Logitar.Cms.Web/Filters/ExceptionHandling.cs
@@ -0,0 +1,35 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using Logitar.Cms.Contracts.Errors;
+using Logitar.Cms.Core;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Logitar.Cms.Web.Filters;
+
+public class ExceptionHandling : ExceptionFilterAttribute
+{
+  public override void OnException(ExceptionContext context)
+  {
+    if (context.Exception is ValidationException validation)
+    {
+      ValidationError error = new();
+      foreach (ValidationFailure failure in validation.Errors)
+      {
+        error.Add(failure.ToPropertyError());
+      }
+
+      context.Result = new BadRequestObjectResult(error);
+      context.ExceptionHandled = true;
+    }
+    else if (context.Exception is ConflictException conflict)
+    {
+      context.Result = new ConflictObjectResult(conflict.Error);
+      context.ExceptionHandled = true;
+    }
+    else
+    {
+      base.OnException(context);
+    }
+  }
+}
