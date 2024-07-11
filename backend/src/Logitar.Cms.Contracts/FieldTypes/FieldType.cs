e14885e04eba78ae7e13de474b9612f6c01d5ecc
Francis Pion <francispion@hotmail.com>
2024-07-11 15:12:59 -0400

    Contracts.

---
+++ backend/src/Logitar.Cms.Contracts/FieldTypes/FieldType.cs
@@ -0,0 +1,24 @@
+﻿using Logitar.Cms.Contracts.FieldTypes.Properties;
+
+namespace Logitar.Cms.Contracts.FieldTypes;
+
+public class FieldType : Aggregate
+{
+  public string UniqueName { get; set; }
+  public string? DisplayName { get; set; }
+  public string? Description { get; set; }
+
+  public DataType DataType { get; set; }
+  public StringProperties? StringProperties { get; set; }
+
+  public FieldType() : this(string.Empty)
+  {
+  }
+
+  public FieldType(string uniqueName)
+  {
+    UniqueName = uniqueName;
+  }
+
+  public override string ToString() => $"{DisplayName ?? UniqueName} | {base.ToString()}";
+}
---
+++ backend/src/Logitar.Cms.Contracts/FieldTypes/Properties/StringProperties.cs
@@ -0,0 +1,23 @@
+﻿namespace Logitar.Cms.Contracts.FieldTypes.Properties;
+
+public record StringProperties : IStringProperties
+{
+  public int? MinimumLength { get; set; }
+  public int? MaximumLength { get; set; }
+  public string? Pattern { get; set; }
+
+  public StringProperties() : this(minimumLength: null, maximumLength: null, pattern: null)
+  {
+  }
+
+  public StringProperties(IStringProperties @string) : this(@string.MinimumLength, @string.MaximumLength, @string.Pattern)
+  {
+  }
+
+  public StringProperties(int? minimumLength, int? maximumLength, string? pattern)
+  {
+    MinimumLength = minimumLength;
+    MaximumLength = maximumLength;
+    Pattern = pattern;
+  }
+}
