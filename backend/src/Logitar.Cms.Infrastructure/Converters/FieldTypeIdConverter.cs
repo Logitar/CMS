8766c3f61c9b5798a758e93708c044f74c5d1964
Francis Pion <francispion@hotmail.com>
2024-07-11 15:15:01 -0400

    Infrastructure.

---
+++ backend/src/Logitar.Cms.Infrastructure/Converters/FieldTypeIdConverter.cs
@@ -0,0 +1,16 @@
+﻿using Logitar.Cms.Core.FieldTypes;
+
+namespace Logitar.Cms.Infrastructure.Converters;
+
+public class FieldTypeIdConverter : JsonConverter<FieldTypeId>
+{
+  public override FieldTypeId? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+  {
+    return FieldTypeId.TryCreate(reader.GetString());
+  }
+
+  public override void Write(Utf8JsonWriter writer, FieldTypeId fieldTypeId, JsonSerializerOptions options)
+  {
+    writer.WriteStringValue(fieldTypeId.Value);
+  }
+}
