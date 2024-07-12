1018915e918eadea741d90f9daa3967cfd2832d3
Francis Pion <francispion@hotmail.com>
2024-07-11 23:29:25 -0400

    Validator.

--- backend/src/Logitar.Cms.Core/FieldTypes/Validators/StringPropertiesValidator.cs
+++ backend/src/Logitar.Cms.Core/FieldTypes/Validators/StringPropertiesValidator.cs
@@ -7,6 +7,14 @@ public class StringPropertiesValidator : AbstractValidator<IStringProperties>
 {
   public StringPropertiesValidator()
   {
-    // TODO(fpion): implement
+    When(x => x.MinimumLength.HasValue && x.MaximumLength.HasValue, () =>
+    {
+      RuleFor(x => x.MinimumLength!.Value).GreaterThan(0).LessThanOrEqualTo(x => x.MaximumLength!.Value);
+      RuleFor(x => x.MaximumLength!.Value).GreaterThanOrEqualTo(x => x.MaximumLength!.Value);
+    }).Otherwise(() =>
+    {
+      When(x => x.MinimumLength.HasValue, () => RuleFor(x => x.MinimumLength!.Value).GreaterThan(0));
+      When(x => x.MaximumLength.HasValue, () => RuleFor(x => x.MaximumLength!.Value).GreaterThan(0));
+    });
   }
 }
