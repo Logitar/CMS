7a359947e79b6aef97adfddbbbdb0512554c80e3
Francis Pion <francispion@hotmail.com>
2024-07-13 12:36:53 -0400

    Contracts.

---
+++ backend/src/Logitar.Cms.Contracts/Contents/FieldValuePayload.cs
@@ -0,0 +1,17 @@
+﻿namespace Logitar.Cms.Contracts.Contents;
+
+public record FieldValuePayload
+{
+  public Guid Id { get; set; }
+  public string Value { get; set; }
+
+  public FieldValuePayload() : this(Guid.Empty, string.Empty)
+  {
+  }
+
+  public FieldValuePayload(Guid id, string value)
+  {
+    Id = id;
+    Value = value;
+  }
+}
--- backend/src/Logitar.Cms.Contracts/Contents/SaveContentLocalePayload.cs
+++ backend/src/Logitar.Cms.Contracts/Contents/SaveContentLocalePayload.cs
@@ -4,12 +4,16 @@ public class SaveContentLocalePayload
 {
   public string UniqueName { get; set; }
 
+  public List<FieldValuePayload> Fields { get; set; }
+
   public SaveContentLocalePayload() : this(string.Empty)
   {
   }
 
-  public SaveContentLocalePayload(string uniqueName)
+  public SaveContentLocalePayload(string uniqueName, IEnumerable<FieldValuePayload>? fields = null)
   {
     UniqueName = uniqueName;
+
+    Fields = fields?.ToList() ?? [];
   }
 }
