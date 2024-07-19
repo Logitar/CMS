75a4dae957ba38d4468fd9772c739b86ea5448b9
Francis Pion <francispion@hotmail.com>
2024-07-19 00:37:01 -0400

    Persistence.

---
+++ backend/src/Logitar.Cms.EntityFrameworkCore/QueryingExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Logitar.Cms.Contracts.Search;
+using Logitar.Data;
+
+namespace Logitar.Cms.EntityFrameworkCore;
+
+internal static class QueryingExtensions
+{
+  public static IQueryBuilder ApplyIdInFilter(this IQueryBuilder builder, ColumnId column, SearchPayload payload)
+  {
+    if (payload.IdIn == null || payload.IdIn.Count < 1)
+    {
+      return builder;
+    }
+
+    Guid[] uniqueIds = payload.IdIn.Distinct().ToArray();
+
+    return builder.Where(column, Operators.IsIn(uniqueIds));
+  }
+
+  public static IQueryable<T> ApplyPaging<T>(this IQueryable<T> query, SearchPayload payload)
+  {
+    if (payload.Skip.HasValue)
+    {
+      query = query.Skip(payload.Skip.Value);
+    }
+
+    if (payload.Limit.HasValue)
+    {
+      query = query.Take(payload.Limit.Value);
+    }
+
+    return query;
+  }
+}
