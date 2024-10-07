952668244a859db3b99863a69b85f7635f44c7c7
Francis Pion <francispion@hotmail.com>
2024-10-07 13:49:41 -0400

    infrastructure

---
+++ backend/src/Logitar.Cms.Infrastructure/DependencyInjectionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Logitar.Cms.Core;
+using Logitar.Cms.Infrastructure.Converters;
+using Logitar.EventSourcing.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Logitar.Cms.Infrastructure;
+
+public static class DependencyInjectionExtensions
+{
+  public static IServiceCollection AddPriceWatchInfrastructure(this IServiceCollection services)
+  {
+    return services
+      .AddLogitarEventSourcingInfrastructure()
+      .AddLogitarCmsCore()
+      .AddSingleton<IEventSerializer>(InitializeEventSerializer)
+      .AddTransient<IEventBus, EventBus>();
+  }
+
+  private static EventSerializer InitializeEventSerializer(IServiceProvider serviceProvider) => new(serviceProvider.GetJsonConverters());
+  public static IEnumerable<JsonConverter> GetJsonConverters(this IServiceProvider _) =>
+  [
+    new LanguageIdConverter(),
+    new LocaleConverter()
+  ];
+}
