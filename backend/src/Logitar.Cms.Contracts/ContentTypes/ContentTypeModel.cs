50d220fc2b88982c8b7c6fc6f12aa63d11a4cf1f
Francis Pion <francispion@hotmail.com>
2024-10-08 18:38:26 -0400

    contracts

---
+++ backend/src/Logitar.Cms.Contracts/ContentTypes/ContentTypeModel.cs
@@ -0,0 +1,21 @@
+﻿namespace Logitar.Cms.Contracts.ContentTypes;
+
+public class ContentTypeModel : AggregateModel
+{
+  public bool IsInvariant { get; set; }
+
+  public string UniqueName { get; set; }
+  public string? DisplayName { get; set; }
+  public string? Description { get; set; }
+
+  public ContentTypeModel() : this(string.Empty)
+  {
+  }
+
+  public ContentTypeModel(string uniqueName)
+  {
+    UniqueName = uniqueName;
+  }
+
+  public override string ToString() => $"{DisplayName ?? UniqueName} (Id={Id})";
+}
---
+++ backend/src/Logitar.Cms.Contracts/ContentTypes/UpdateContentTypePayload.cs
@@ -0,0 +1,8 @@
+﻿namespace Logitar.Cms.Contracts.ContentTypes;
+
+public record UpdateContentTypePayload
+{
+  public string? UniqueName { get; set; }
+  public Change<string>? DisplayName { get; set; }
+  public Change<string>? Description { get; set; }
+}
