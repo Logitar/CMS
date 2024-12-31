7c69ddf231ea9325e2ced1a7fe6795dae8362b76
Francis Pion <francispion@hotmail.com>
2024-12-31 01:12:17 -0500

    SqlServer

---
+++ backend/src/Logitar.Cms.Infrastructure.SqlServer/DependencyInjectionExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Logitar.Identity.EntityFrameworkCore.SqlServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Logitar.Cms.Infrastructure.SqlServer;
+
+public static class DependencyInjectionExtensions
+{
+  private const string ConfigurationKey = "SQLCONNSTR_Cms";
+
+  public static IServiceCollection AddLogitarCmsWithSqlServer(this IServiceCollection services, IConfiguration configuration)
+  {
+    string? connectionString = Environment.GetEnvironmentVariable(ConfigurationKey);
+    if (string.IsNullOrWhiteSpace(connectionString))
+    {
+      connectionString = configuration.GetValue<string>(ConfigurationKey);
+    }
+    if (string.IsNullOrWhiteSpace(connectionString))
+    {
+      throw new ArgumentException($"The configuration '{ConfigurationKey}' could not be found.", nameof(configuration));
+    }
+    return services.AddLogitarCmsWithSqlServer(connectionString.Trim());
+  }
+  public static IServiceCollection AddLogitarCmsWithSqlServer(this IServiceCollection services, string connectionString)
+  {
+    return services
+      .AddDbContext<CmsContext>(options => options.UseSqlServer(connectionString, builder => builder.MigrationsAssembly("Logitar.Cms.Infrastructure.SqlServer")))
+      .AddLogitarIdentityWithEntityFrameworkCoreSqlServer(connectionString)
+      .AddSingleton<IQueryHelper, SqlServerQueryHelper>();
+  }
+}
