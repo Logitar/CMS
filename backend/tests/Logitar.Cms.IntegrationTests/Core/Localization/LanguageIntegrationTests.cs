e0194014c1b5d581d771e8a6b81346853f42c643
Francis Pion <francispion@hotmail.com>
2025-01-02 15:35:16 -0500

    languages

---
+++ backend/tests/Logitar.Cms.IntegrationTests/Core/Localization/LanguageIntegrationTests.cs
@@ -0,0 +1,40 @@
+﻿using Logitar.Cms.Core.Localization.Commands;
+using Logitar.Cms.Core.Localization.Models;
+using Logitar.Cms.Infrastructure;
+
+namespace Logitar.Cms.Core.Localization;
+
+[Trait(Traits.Category, Categories.Integration)]
+public class LanguageIntegrationTests : IntegrationTests
+{
+  public LanguageIntegrationTests() : base(DatabaseProvider.SqlServer)
+  {
+  }
+
+  [Theory(DisplayName = "It should create a new language.")]
+  [InlineData(null)]
+  [InlineData("36393a67-548d-4366-be96-71dddc7a8327")]
+  public async Task Given_NotExist_When_Create_Then_LanguageCreated(string? idValue)
+  {
+    Guid? id = idValue == null ? null : Guid.Parse(idValue);
+
+    CreateOrReplaceLanguagePayload payload = new("fr");
+    CreateOrReplaceLanguageCommand command = new(id, payload, Version: null);
+    CreateOrReplaceLanguageResult result = await Mediator.Send(command);
+    Assert.True(result.Created);
+
+    LanguageModel? language = result.Language;
+    Assert.NotNull(language);
+    if (id.HasValue)
+    {
+      Assert.Equal(id.Value, language.Id);
+    }
+    Assert.Equal(1, language.Version);
+    Assert.Equal(Actor, language.CreatedBy);
+    Assert.Equal(DateTime.UtcNow, language.CreatedOn, TimeSpan.FromSeconds(1));
+    Assert.Equal(Actor, language.UpdatedBy);
+    Assert.Equal(DateTime.UtcNow, language.UpdatedOn, TimeSpan.FromSeconds(1));
+    Assert.False(language.IsDefault);
+    Assert.Equal(payload.Locale, language.Locale.Code);
+  }
+}
---
+++ backend/tests/Logitar.Cms.IntegrationTests/IntegrationTests.cs
@@ -0,0 +1,99 @@
+﻿using Logitar.Cms.Core;
+using Logitar.Cms.Core.Actors;
+using Logitar.Cms.Core.Commands;
+using Logitar.Cms.Infrastructure;
+using Logitar.Cms.Infrastructure.Commands;
+using Logitar.Cms.Infrastructure.SqlServer;
+using Logitar.Data;
+using Logitar.Data.SqlServer;
+using Logitar.EventSourcing.EntityFrameworkCore.Relational;
+using Logitar.Identity.EntityFrameworkCore.Relational;
+using Logitar.Identity.EntityFrameworkCore.Relational.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Text;
+using CmsDb = Logitar.Cms.Infrastructure.CmsDb;
+using IdentityDb = Logitar.Identity.EntityFrameworkCore.Relational.IdentityDb;
+
+namespace Logitar.Cms;
+
+public abstract class IntegrationTests : IAsyncLifetime
+{
+  private const string DefaultUniqueName = "admin";
+  private const string DefaultPassword = "P@s$W0rD";
+  private const string DefaultLocale = "en";
+
+  private readonly DatabaseProvider _databaseProvider;
+
+  protected IConfiguration Configuration { get; }
+  protected IServiceProvider ServiceProvider { get; }
+
+  protected IMediator Mediator { get; }
+  protected CmsContext CmsContext { get; }
+
+  private readonly TestContext _context = new();
+  protected ActorModel Actor => _context.Actor ?? new();
+
+  protected IntegrationTests(DatabaseProvider databaseProvider)
+  {
+    _databaseProvider = databaseProvider;
+
+    Configuration = new ConfigurationBuilder()
+      .AddJsonFile("appsettings.json", optional: false, reloadOnChange: false)
+      .Build();
+
+    ServiceCollection services = new();
+    services.AddSingleton(Configuration);
+
+    services.AddLogitarCmsCore();
+    services.AddLogitarCmsInfrastructure();
+    switch (databaseProvider)
+    {
+      case DatabaseProvider.SqlServer:
+        string connectionString = Configuration.GetValue<string>("SQLCONNSTR_Cms")?.Replace("{Database}", GetType().Name) ?? string.Empty;
+        services.AddLogitarCmsWithSqlServer(connectionString);
+        break;
+      default:
+        throw new DatabaseProviderNotSupportedException(databaseProvider);
+    }
+
+    services.AddSingleton<IApplicationContext, TestApplicationContext>();
+    services.AddSingleton(_context);
+    ServiceProvider = services.BuildServiceProvider();
+
+    Mediator = ServiceProvider.GetRequiredService<IMediator>();
+    CmsContext = ServiceProvider.GetRequiredService<CmsContext>();
+  }
+
+  public virtual async Task InitializeAsync()
+  {
+    await Mediator.Send(new InitializeDatabaseCommand());
+
+    StringBuilder sql = new();
+    TableId[] tables = [EventDb.Streams.Table, IdentityDb.Actors.Table, IdentityDb.Users.Table, CmsDb.Languages.Table, CmsDb.FieldTypes.Table];
+    foreach (TableId table in tables)
+    {
+      ICommand command = CreateDeleteBuilder(table).Build();
+      sql.Append(command.Text).Append(';').AppendLine();
+    }
+    await CmsContext.Database.ExecuteSqlRawAsync(sql.ToString());
+
+    string uniqueName = Configuration.GetValue<string>("CMS_USERNAME") ?? DefaultUniqueName;
+    string password = Configuration.GetValue<string>("CMS_PASSWORD") ?? DefaultPassword;
+    string defaultLocale = Configuration.GetValue<string>("CMS_LOCALE") ?? DefaultLocale;
+    await Mediator.Send(new InitializeCmsCommand(uniqueName, password, defaultLocale));
+
+    IdentityContext identityContext = ServiceProvider.GetRequiredService<IdentityContext>();
+    ActorEntity actor = await identityContext.Actors.AsNoTracking().SingleAsync();
+    _context.Actor = Mapper.ToActor(actor);
+  }
+  protected IDeleteBuilder CreateDeleteBuilder(TableId table) => _databaseProvider switch
+  {
+    DatabaseProvider.SqlServer => new SqlServerDeleteBuilder(table),
+    _ => throw new DatabaseProviderNotSupportedException(_databaseProvider),
+  };
+
+  public Task DisposeAsync() => Task.CompletedTask;
+}
