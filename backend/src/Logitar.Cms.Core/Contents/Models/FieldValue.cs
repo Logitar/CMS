48706de44614b99db19f5f0aec60d02abb30e9b5
Francis Pion <francispion@hotmail.com>
2025-01-04 11:56:43 -0500

    FieldValue

---
+++ backend/src/Logitar.Cms.Core/Contents/Models/FieldValue.cs
@@ -0,0 +1,23 @@
+﻿namespace Logitar.Cms.Core.Contents.Models;
+
+public record FieldValue
+{
+  public Guid Id { get; set; }
+  public string Value { get; set; } = string.Empty;
+
+  public FieldValue()
+  {
+  }
+
+  public FieldValue(KeyValuePair<Guid, string> pair)
+  {
+    Id = pair.Key;
+    Value = pair.Value;
+  }
+
+  public FieldValue(Guid id, string value)
+  {
+    Id = id;
+    Value = value;
+  }
+}
--- backend/src/Logitar.Cms.Core/Contents/Models/SaveContentLocalePayload.cs
+++ backend/src/Logitar.Cms.Core/Contents/Models/SaveContentLocalePayload.cs
@@ -5,4 +5,6 @@ public record SaveContentLocalePayload
   public string UniqueName { get; set; } = string.Empty;
   public string? DisplayName { get; set; }
   public string? Description { get; set; }
+
+  public List<FieldValue> FieldValues { get; set; } = [];
 }
