f82fd2d15684870387a616d4e35de8ec45b84fa3
Francis Pion <francispion@hotmail.com>
2025-01-05 12:14:40 -0500

    payload+validator+controller

---
+++ backend/src/Logitar.Cms.Core/Contents/Models/CreateOrReplaceContentPayload.cs
@@ -0,0 +1,10 @@
+﻿namespace Logitar.Cms.Core.Contents.Models;
+
+public record CreateOrReplaceContentPayload
+{
+  public Guid? ContentTypeId { get; set; }
+
+  public string UniqueName { get; set; } = string.Empty;
+  public string? DisplayName { get; set; }
+  public string? Description { get; set; }
+}
---
+++ backend/src/Logitar.Cms.Core/Contents/Validators/CreateOrReplaceContentValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using Logitar.Cms.Core.Contents.Models;
+using Logitar.Identity.Core;
+
+namespace Logitar.Cms.Core.Contents.Validators;
+
+internal class CreateOrReplaceContentValidator : AbstractValidator<CreateOrReplaceContentPayload>
+{
+  public CreateOrReplaceContentValidator()
+  {
+    RuleFor(x => x.UniqueName).UniqueName(Content.UniqueNameSettings);
+    When(x => !string.IsNullOrWhiteSpace(x.DisplayName), () => RuleFor(x => x.DisplayName!).DisplayName());
+    When(x => !string.IsNullOrWhiteSpace(x.Description), () => RuleFor(x => x.Description!).Description());
+  }
+}
