b9cf180220a1ea0db2fbdd035349c661dfaf94e6
Francis Pion <francispion@hotmail.com>
2025-01-06 17:01:56 -0500

    todo

--- backend/src/Logitar.Cms.Core/Contents/Models/UpdateContentPayload.cs
+++ backend/src/Logitar.Cms.Core/Contents/Models/UpdateContentPayload.cs
@@ -5,4 +5,6 @@ public record UpdateContentPayload
   public string? UniqueName { get; set; }
   public ChangeModel<string>? DisplayName { get; set; }
   public ChangeModel<string>? Description { get; set; }
+
+  // TODO(fpion): FieldValues
 }
