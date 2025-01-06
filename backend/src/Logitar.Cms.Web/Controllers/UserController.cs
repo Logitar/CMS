c4e9b0730070464d50ec4ad29d7cdf419789dfd1
Francis Pion <francispion@hotmail.com>
2025-01-05 20:40:10 -0500

    UserController

---
+++ backend/src/Logitar.Cms.Web/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using Logitar.Cms.Core.Users.Commands;
+using Logitar.Cms.Core.Users.Models;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Logitar.Cms.Web.Controllers;
+
+[ApiController]
+[Authorize]
+[Route("api/users")]
+public class UserController : ControllerBase
+{
+  private readonly IMediator _mediator;
+
+  public UserController(IMediator mediator)
+  {
+    _mediator = mediator;
+  }
+
+  [HttpPatch("authenticate")]
+  public async Task<ActionResult<UserModel>> AuthenticateAsync([FromBody] AuthenticateUserPayload payload, CancellationToken cancellationToken)
+  {
+    UserModel user = await _mediator.Send(new AuthenticateUserCommand(payload), cancellationToken);
+    return Ok(user);
+  }
+}
