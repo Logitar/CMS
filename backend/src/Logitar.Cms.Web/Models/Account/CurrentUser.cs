fda1a49873f585717230b03ead06ea9332f14368
Francis Pion <francispion@hotmail.com>
2025-01-05 20:46:30 -0500

    AccountController

---
+++ backend/src/Logitar.Cms.Web/Models/Account/CurrentUser.cs
@@ -0,0 +1,26 @@
+﻿using Logitar.Cms.Core.Sessions.Models;
+using Logitar.Cms.Core.Users.Models;
+
+namespace Logitar.Cms.Web.Models.Account;
+
+public record CurrentUser
+{
+  public string DisplayName { get; set; } = string.Empty;
+  public string? EmailAddress { get; set; }
+  public string? PictureUrl { get; set; }
+
+  public CurrentUser()
+  {
+  }
+
+  public CurrentUser(SessionModel session) : this(session.User)
+  {
+  }
+
+  public CurrentUser(UserModel user)
+  {
+    DisplayName = user.FullName ?? user.UniqueName;
+    EmailAddress = user.Email?.Address;
+    PictureUrl = user.Picture;
+  }
+}
---
+++ backend/src/Logitar.Cms.Web/Models/Account/SignInPayload.cs
@@ -0,0 +1,7 @@
+﻿namespace Logitar.Cms.Web.Models.Account;
+
+public record SignInPayload
+{
+  public string Username { get; set; } = string.Empty;
+  public string Password { get; set; } = string.Empty;
+}
