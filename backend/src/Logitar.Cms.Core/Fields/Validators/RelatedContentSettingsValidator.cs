2e24e152931f9221859c722e13a49b06586bee32
Francis Pion <francispion@hotmail.com>
2025-01-08 01:32:35 -0500

    fix

--- backend/src/Logitar.Cms.Core/Fields/Validators/RelatedContentSettingsValidator.cs
+++ backend/src/Logitar.Cms.Core/Fields/Validators/RelatedContentSettingsValidator.cs
@@ -3,9 +3,4 @@ using Logitar.Cms.Core.Fields.Models;
 
 namespace Logitar.Cms.Core.Fields.Validators;
 
-internal class RelatedContentSettingsValidator : AbstractValidator<RelatedContentSettingsModel>
-{
-  public RelatedContentSettingsValidator()
-  {
-  }
-}
+internal class RelatedContentSettingsValidator : AbstractValidator<RelatedContentSettingsModel>;
