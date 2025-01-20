14f4e858b27a70787b32a47958612c49ec016147
Francis Pion <francispion@hotmail.com>
2025-01-20 10:38:49 -0500

    rename

--- backend/tests/Logitar.Cms.UnitTests/Core/Contents/Commands/UpdateContentCommandHandlerTests.cs
+++ backend/tests/Logitar.Cms.UnitTests/Core/Contents/Commands/UpdateContentCommandHandlerTests.cs
@@ -34,8 +34,8 @@ public class UpdateContentCommandHandlerTests
   [Fact(DisplayName = "It should return null when the content was not found.")]
   public async Task Given_ContentNotFound_When_Handle_Then_NullReturned()
   {
-    UpdateContentLocalePayload payload = new();
-    UpdateContentLocaleCommand command = new(Guid.NewGuid(), LanguageId: null, payload);
+    UpdateContentPayload payload = new();
+    UpdateContentCommand command = new(Guid.NewGuid(), LanguageId: null, payload);
     ContentModel? content = await _handler.Handle(command, _cancellationToken);
     Assert.Null(content);
   }
@@ -50,8 +50,8 @@ public class UpdateContentCommandHandlerTests
     Language language = new(new Locale("en"), isDefault: true);
     _languageRepository.Setup(x => x.LoadAsync(language.Id, _cancellationToken)).ReturnsAsync(language);
 
-    UpdateContentLocalePayload payload = new();
-    UpdateContentLocaleCommand command = new(content.Id.ToGuid(), language.Id.ToGuid(), payload);
+    UpdateContentPayload payload = new();
+    UpdateContentCommand command = new(content.Id.ToGuid(), language.Id.ToGuid(), payload);
     ContentModel? result = await _handler.Handle(command, _cancellationToken);
     Assert.Null(result);
   }
@@ -63,8 +63,8 @@ public class UpdateContentCommandHandlerTests
     Content content = new(_contentType, invariant);
     _contentRepository.Setup(x => x.LoadAsync(content.Id, _cancellationToken)).ReturnsAsync(content);
 
-    UpdateContentLocalePayload payload = new();
-    UpdateContentLocaleCommand command = new(content.Id.ToGuid(), Guid.NewGuid(), payload);
+    UpdateContentPayload payload = new();
+    UpdateContentCommand command = new(content.Id.ToGuid(), Guid.NewGuid(), payload);
     var exception = await Assert.ThrowsAsync<LanguageNotFoundException>(async () => await _handler.Handle(command, _cancellationToken));
     Assert.True(command.LanguageId.HasValue);
     Assert.Equal(command.LanguageId.Value, exception.LanguageId);
@@ -74,12 +74,12 @@ public class UpdateContentCommandHandlerTests
   [Fact(DisplayName = "It should throw ValidationException when the payload is not valid.")]
   public async Task Given_InvalidPayload_When_Handle_Then_ValidationException()
   {
-    UpdateContentLocalePayload payload = new()
+    UpdateContentPayload payload = new()
     {
       UniqueName = "MyBlogArticle!",
       DisplayName = new ChangeModel<string>(RandomStringGenerator.GetString(999))
     };
-    UpdateContentLocaleCommand command = new(Guid.NewGuid(), LanguageId: null, payload);
+    UpdateContentCommand command = new(Guid.NewGuid(), LanguageId: null, payload);
     var exception = await Assert.ThrowsAsync<ValidationException>(async () => await _handler.Handle(command, _cancellationToken));
 
     Assert.Equal(2, exception.Errors.Count());
