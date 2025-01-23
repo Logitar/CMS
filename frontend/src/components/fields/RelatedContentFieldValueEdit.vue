71b60ce41fc85d76c90ba640b19258220648341e
Francis Pion <francispion@hotmail.com>
2025-01-23 17:21:01 -0500

    Implemented Multi-content select. (#92)

--- frontend/src/components/fields/RelatedContentFieldValueEdit.vue
+++ frontend/src/components/fields/RelatedContentFieldValueEdit.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { computed } from "vue";
 
+import ContentMultiselect from "@/components/contents/ContentMultiselect.vue";
 import ContentSelect from "@/components/contents/ContentSelect.vue";
 import type { FieldDefinition, FieldType } from "@/types/fields";
 import type { Language } from "@/types/languages";
@@ -11,15 +12,34 @@ const props = defineProps<{
   modelValue?: string;
 }>();
 
+const contentIds = computed<string[] | undefined>(() => (props.modelValue ? JSON.parse(props.modelValue) : undefined));
 const fieldType = computed<FieldType>(() => props.definition.fieldType);
 
-defineEmits<{
+const emit = defineEmits<{
   (e: "update:model-value", value: string): void;
 }>();
+
+function onContentIdsUpdate(contentIds: string[]): void {
+  emit("update:model-value", JSON.stringify(contentIds));
+}
 </script>
 
 <template>
+  <ContentMultiselect
+    v-if="fieldType.relatedContent?.isMultiple"
+    :content-type-id="fieldType.relatedContent?.contentTypeId"
+    :id="definition.id"
+    :label="definition.displayName ?? definition.uniqueName"
+    :language-id="language?.id"
+    :model-value="contentIds"
+    :name="definition.uniqueName"
+    :placeholder="definition.placeholder ?? definition.displayName ?? definition.uniqueName"
+    raw
+    :required="definition.isRequired"
+    @update:model-value="onContentIdsUpdate"
+  />
   <ContentSelect
+    v-else
     :content-type-id="fieldType.relatedContent?.contentTypeId"
     :id="definition.id"
     :label="definition.displayName ?? definition.uniqueName"
@@ -31,5 +51,4 @@ defineEmits<{
     :required="definition.isRequired"
     @update:model-value="$emit('update:model-value', $event ?? '')"
   />
-  <!-- TODO(fpion): multiple -->
 </template>
