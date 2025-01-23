6f984ec73497165c87016d86ab40901bc9fdecf5
Francis Pion <francispion@hotmail.com>
2025-01-23 16:51:05 -0500

    Implemented Multiselect field values. (#90)

---
+++ frontend/src/components/shared/AppMultiselect.vue
@@ -0,0 +1,99 @@
+<script setup lang="ts">
+import type { SelectOption } from "logitar-vue3-ui";
+import { computed } from "vue";
+import { nanoid } from "nanoid";
+import { parsingUtils } from "logitar-js";
+import { useI18n } from "vue-i18n";
+
+import AppTextarea from "./AppTextarea.vue";
+
+const { parseBoolean } = parsingUtils;
+const { t } = useI18n();
+
+const props = withDefaults(
+  defineProps<{
+    id?: string;
+    label?: string;
+    modelValue?: string[];
+    name?: string;
+    options?: SelectOption[];
+    placeholder?: string;
+    raw?: boolean | string;
+    required?: boolean | string;
+  }>(),
+  {
+    id: () => nanoid(),
+    modelValue: () => [],
+    options: () => [],
+  },
+);
+
+const formattedValue = computed<string>(() => props.modelValue.map((value) => indexedOptions.value.get(value) ?? value).join("\n"));
+const indexedOptions = computed<Map<string, string>>(() => {
+  const index = new Map<string, string>();
+  props.options.forEach((option) => {
+    const text: string | undefined = option.label ?? option.text;
+    const value: string | undefined = option.value ?? option.text;
+    if (text && value) {
+      index.set(value, text);
+    }
+  });
+  return index;
+});
+const isRaw = computed<boolean>(() => parseBoolean(props.raw) ?? false);
+const rows = computed<number>(() => 1 + props.modelValue.length);
+
+const emit = defineEmits<{
+  (e: "update:model-value", values: string[]): void;
+}>();
+
+function isSelected(option: SelectOption): boolean {
+  const value: string | undefined = option.value ?? option.text;
+  return Boolean(value && props.modelValue.includes(value));
+}
+function toggle(option: SelectOption): void {
+  const value: string | undefined = option.value ?? option.text;
+  if (value) {
+    const selected: string[] = [...props.modelValue];
+    const index: number = selected.findIndex((val) => val === value);
+    if (index < 0) {
+      selected.push(value);
+    } else {
+      selected.splice(index, 1);
+    }
+    emit("update:model-value", selected);
+  }
+}
+</script>
+
+<template>
+  <div class="dropdown">
+    <AppTextarea
+      aria-expanded="false"
+      data-bs-auto-close="outside"
+      data-bs-toggle="dropdown"
+      floating
+      :id="id"
+      :label="label"
+      :model-value="formattedValue"
+      :name="name"
+      :placeholder="label"
+      :raw="raw"
+      readonly
+      :required="required"
+      :rows="rows"
+    />
+    <ul class="dropdown-menu">
+      <li v-if="placeholder" class="disabled dropdown-item">{{ isRaw ? placeholder : t(placeholder) }}</li>
+      <li v-for="option in options" :key="option.value" :class="{ active: isSelected(option), 'dropdown-item': true }" @click.prevent="toggle(option)">
+        <font-awesome-icon v-if="isSelected(option)" icon="fas fa-check" /> {{ option.text }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+.dropdown-item {
+  cursor: pointer;
+}
+</style>
--- frontend/src/views/contents/ContentEdit.vue
+++ frontend/src/views/contents/ContentEdit.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { TarTab, TarTabs } from "logitar-vue3-ui";
-import { arrayUtils } from "logitar-js";
 import { computed, inject, onMounted, ref } from "vue";
 import { useI18n } from "vue-i18n";
 import { useRoute, useRouter } from "vue-router";
@@ -10,7 +9,6 @@ import StatusDetail from "@/components/shared/StatusDetail.vue";
 import type { ApiError } from "@/types/api";
 import type { Content, ContentLocale } from "@/types/contents";
 import { StatusCodes } from "@/enums/statusCodes";
-import { formatLanguage } from "@/helpers/format";
 import { handleErrorKey } from "@/inject/App";
 import { readContent } from "@/api/contents";
 import { useToastStore } from "@/stores/toast";
@@ -19,19 +17,11 @@ const handleError = inject(handleErrorKey) as (e: unknown) => void;
 const route = useRoute();
 const router = useRouter();
 const toasts = useToastStore();
-const { orderBy } = arrayUtils;
 const { t } = useI18n();
 
 const content = ref<Content>();
 
-// const locales = computed<ContentLocale[]>(() =>
-//   content.value
-//     ? orderBy(
-//         content.value.locales.filter((locale) => Boolean(locale.language)),
-//         "language.locale.displayName",
-//       )
-//     : [],
-// ); // TODO(fpion): locales
+const locales = computed<ContentLocale[]>(() => (content.value ? content.value.locales.filter((locale) => Boolean(locale.language)) : [])); // TODO(fpion): sorting
 
 function setModel(model: Content): void {
   content.value = model;
@@ -69,9 +59,9 @@ onMounted(async () => {
         <TarTab active id="invariant" :title="t('contents.items.invariant')">
           <ContentLocaleEdit :content="content" :locale="content.invariant" @error="handleError" @saved="onSaved" />
         </TarTab>
-        <!-- <TarTab v-for="locale in locales" :key="locale.language?.id" :id="locale.language?.id" :title="formatLanguage(locale.language)">
+        <TarTab v-for="locale in locales" :key="locale.language?.id" :id="locale.language?.id" :title="locale.language?.locale.displayName">
           <ContentLocaleEdit :content="content" :locale="locale" @error="handleError" @saved="onSaved" />
-        </TarTab> -->
+        </TarTab>
       </TarTabs>
     </template>
   </main>
