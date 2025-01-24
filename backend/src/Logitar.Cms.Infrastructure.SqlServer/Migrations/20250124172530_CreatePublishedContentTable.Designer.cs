32454b9d9315ee9351d68d78f6e6a77e15ce1ba3
Francis Pion <francispion@hotmail.com>
2025-01-24 12:26:16 -0500

    migration

--- backend/src/Logitar.Cms.Infrastructure.SqlServer/Migrations/20250124172530_CreatePublishedContentTable.Designer.cs
+++ backend/src/Logitar.Cms.Infrastructure.SqlServer/Migrations/20250124172530_CreatePublishedContentTable.Designer.cs
@@ -3,6 +3,7 @@ using Logitar.Cms.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Logitar.Cms.Infrastructure.SqlServer.Migrations
 {
     [DbContext(typeof(CmsContext))]
-    partial class CmsContextModelSnapshot : ModelSnapshot
+    [Migration("20250124172530_CreatePublishedContentTable")]
+    partial class CreatePublishedContentTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
