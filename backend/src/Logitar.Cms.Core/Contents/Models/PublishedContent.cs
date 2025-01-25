67235f0c64a75e8450f6d0e2deb94f101506fd55
Francis Pion <francispion@hotmail.com>
2025-01-25 01:07:52 -0500

    todo

--- backend/src/Logitar.Cms.Core/Contents/Models/PublishedContent.cs
+++ backend/src/Logitar.Cms.Core/Contents/Models/PublishedContent.cs
@@ -1,3 +1,28 @@
 ﻿namespace Logitar.Cms.Core.Contents.Models;
 
-public class PublishedContent; // TODO(fpion): implement
+public class PublishedContent // TODO(fpion): implement
+{
+  //public Guid Id { get; set; }
+  //public long Version { get; set; }
+
+  //public ActorModel CreatedBy { get; set; } = new();
+  //public DateTime CreatedOn { get; set; }
+
+  //public ActorModel UpdatedBy { get; set; } = new();
+  //public DateTime UpdatedOn { get; set; }
+
+  //public ContentTypeModel ContentType { get; set; } = new();
+
+  //public ContentLocaleModel Invariant { get; set; }
+  //public List<ContentLocaleModel> Locales { get; set; } = [];
+
+  //public ContentModel()
+  //{
+  //  Invariant = new(this);
+  //}
+
+  //public override bool Equals(object? obj) => obj is AggregateModel aggregate && aggregate.Id == Id;
+  //public override int GetHashCode() => Id.GetHashCode();
+  //public override string ToString() => $"{GetType()} (Id={Id})";
+  //public override string ToString() => $"{Invariant} | {base.ToString()}";
+}
