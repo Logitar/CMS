8009ca17bd99c25d4c6d10b5b2878f564e17bd6c
Francis Pion <francispion@hotmail.com>
2025-01-27 14:48:13 -0500

    Do not include favicon with CMS assets. (#141)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -22,9 +22,9 @@ export default defineConfig({
     emptyOutDir: true,
     rollupOptions: {
       output: {
-        entryFileNames: `assets/[name].js`,
-        chunkFileNames: `assets/[name].js`,
-        assetFileNames: `assets/[name].[ext]`,
+        entryFileNames: "assets/[name].js",
+        chunkFileNames: "assets/[name].js",
+        assetFileNames: "assets/[name].[ext]",
       },
     },
   },
